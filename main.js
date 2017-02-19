e61d44902f5243e1b82d4462066bebba1897a18c
tooncool64 <mdallytc64@gmail.com>
2017-02-19 13:14:36 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var cookies = 0;
 
 function cookieClick(number){
-    cookies = Math.pow(7, 21) + cookies
+    cookies = Math.pow(cookies, 21) + cookies
     document.getElementById("cookies").innerHTML = cookies;
 };
