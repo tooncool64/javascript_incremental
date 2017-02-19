e9e6acb5f40ef389ea85f0c2f07be5947543bc4f
tooncool64 <mdallytc64@gmail.com>
2017-02-19 13:10:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ function cookieClick(number){
     cookies = Math.pow(3, 4) + cookies
     document.getElementById("cookies").innerHTML = cookies;
 };
+if cookies < 1000 {
+    console.log("wow");
+}
