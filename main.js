9a46c4b7c396b13e416f3eeb52cadc319ea1c609
tooncool64 <mdallytc64@gmail.com>
2017-02-18 20:59:10 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var cookies = 0;
 
 function cookieClick(number){
-    cookies = cookies + number;
+    cookies = cookies + number * 2;
     document.getElementById("cookies").innerHTML = cookies;
 };
